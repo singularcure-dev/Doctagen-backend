e7b6372bba6032b867c4df9aaaebd002db6d53d7
mzkosia <mz.cosia@gmail.com>
2023-06-01 14:30:33 +0900

    git pull test

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 /**
+ * git pull test
+ */
+
+/**
  * Ping test
  */
 
 router.get("/", (req, res) => {
-    res.json({ status: 200, success: true });
+  res.json({ status: 200, success: true });
 });
 
 module.exports = router;
